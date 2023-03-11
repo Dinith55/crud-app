b2c2a3470ea791326813871a1e3c8b83544be245
Dinith55 <dinithrodrigo55@gmail.com>
2023-03-11 20:48:39 +0530

    Test Project

---
+++ crud-demo-test/src/main/java/com/crud/demo/test/CrudDemoTest.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.crud.demo.test;
+
+/**
+ *
+ * @author Dinith
+ */
+public class CrudDemoTest {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
